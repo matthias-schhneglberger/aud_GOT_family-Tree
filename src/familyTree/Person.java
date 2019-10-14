ae315a1a97638b626954abe1232a72e5ccf53ef8
Matthias Schneglberger <matthias.schneglberger@gmx.at>
2019-10-14 08:49:53 +0200

    done Person (2. time)

--- src/familyTree/Person.java
+++ src/familyTree/Person.java
@@ -5,10 +5,101 @@
  */
 package familyTree;
 
+import java.util.List;
+import java.util.Objects;
+
 /**
  *
  * @author Fredi
  */
 public class Person {
+    private boolean male;
+    private Person dad;
+    private Person mom;
+    private List<Person> children;
+
+    public Person(boolean male, Person dad, Person mom, List<Person> children) {
+        this.male = male;
+        this.dad = dad;
+        this.mom = mom;
+        this.children = children;
+    }
+
+    public Person(boolean male, Person dad, List<Person> children) {
+        this.male = male;
+        this.dad = dad;
+        this.children = children;
+    }
+
+    public boolean isMale() {
+        return male;
+    }
+
+    public void setMale(boolean male) {
+        this.male = male;
+    }
+
+    public Person getDad() {
+        return dad;
+    }
+
+    public void setDad(Person dad) {
+        this.dad = dad;
+    }
+
+    public Person getMom() {
+        return mom;
+    }
+
+    public void setMom(Person mom) {
+        this.mom = mom;
+    }
+
+    public List<Person> getChildren() {
+        return children;
+    }
+
+    public void setChildren(List<Person> children) {
+        this.children = children;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 97 * hash + (this.male ? 1 : 0);
+        hash = 97 * hash + Objects.hashCode(this.dad);
+        hash = 97 * hash + Objects.hashCode(this.mom);
+        hash = 97 * hash + Objects.hashCode(this.children);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Person other = (Person) obj;
+        if (this.male != other.male) {
+            return false;
+        }
+        if (!Objects.equals(this.dad, other.dad)) {
+            return false;
+        }
+        if (!Objects.equals(this.mom, other.mom)) {
+            return false;
+        }
+        if (!Objects.equals(this.children, other.children)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
     
 }
